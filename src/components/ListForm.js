1bfd0aa87a62e45a9e7e12683df4afbdfa64eb85
Eva Reitzel <evareitzel@gmail.com>
2022-10-26 22:13:13 -0400

    make first form input field controlled; use setState

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -1,11 +1,20 @@
-function ListForm() {
+import {useState} from 'react'
 
+  // POST request to the json server
+  // Use a form to make your post request, specifically a controlled form/component.
+
+function ListForm() {
+  const [newTask, setNewTask] = useState("")
+  
+  function handleChange(e){
+    setNewTask(e.target.value)
+  }
   return (
     <>
       <h1>ListForm</h1>
       <p>Add new tasks: </p>
       <form>
-        <label>Task 1 </label><input></input>
+        <label onChange={handleChange}>Task 1 </label><input></input>
         <label>Task 2 </label><input></input>
         <label>Task 3 </label><input></input>
         <label>Task 4 </label><input></input>
