cf0bb010741b2932227a3fc596e46ef7e0215c0a
Eva Reitzel <evareitzel@gmail.com>
2022-11-01 22:53:48 -0400

    Toggle List  details

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,32 @@
+import {useState} from "react"
 import '../stylesheets/List.css'
 
 function List({listObj}){
   // console.log(listObj)
   const {title, item1, item2, item3, item4} = listObj
+  const [details, setDetails] = useState(false)
+
+  function onBtnClick(){
+    setDetails(!details)
+  } 
+
+  console.log(details)
+
+  const showDetails = 
+  details
+  ? <ul>
+      <li>{item1}</li>
+      <li>{item2}</li>
+      <li>{item3}</li>
+      <li>{item4}</li>
+    </ul>
+  : null
 
   return(
-    <div>
-      <h3 className="title">{title}</h3>
-      <ul>
-        <li>{item1}</li>
-        <li>{item2}</li>
-        <li>{item3}</li>
-        <li>{item4}</li>
-      </ul>
+    <div className="list">
+      <hr />
+      <button onClick={onBtnClick} className ="list-btn">{title}</button>
+      {showDetails}
     </div>
   )
 }
